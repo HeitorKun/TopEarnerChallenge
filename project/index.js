38cc949e62ec04cb70f7a4ab1095a7753847a574
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2024-03-17 13:10:11 -0300

    fetching transactions from API

--- project/index.js
+++ project/index.js
@@ -1,1 +1,12 @@
-console.log("working")
+import { getPreviousYear } from "./transactions/helpers/date.js";
+import { topEarnerTransactions } from "./transactions/services/analytics/index.js";
+import { fetchTransactions } from "./transactions/services/webService/fetchTransactions.js";
+
+const main = async () => {
+  const data = await fetchTransactions();
+  if (!data.transactions) {
+    console.log("Got no Data from the API");
+    return;
+  }
+};
+main();
