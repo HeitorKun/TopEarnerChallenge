1982f0d6688268986e485eae8e617b4fee52255f
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2024-03-18 03:32:14 -0300

    created Task repository

--- src/transactions/repositories/task/TaskRepository.ts
+++ src/transactions/repositories/task/TaskRepository.ts
@@ -1,5 +1,14 @@
-import { Transaction } from "../../entities/Transaction";
 
 export interface ITaskRepository {
-  submitTask(payload: { id: string; result: Transaction[] }): Promise<void>;
+  submitTask(payload: {
+    id: string;
+    transactionIDs: string[];
+  }): Promise<TaskSubmissionStatus>;
+}
+
+export enum TaskSubmissionStatus {
+  Success = 200,
+  BadRequest = 400,
+  NotFound = 404,
+  ServerError = 503
 }
