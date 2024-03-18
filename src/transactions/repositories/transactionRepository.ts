0562839f00372ec4d5dd879708f4f9109bfe777b
Heitor Kunrath <60748906+HeitorKun@users.noreply.github.com>
2024-03-17 22:02:37 -0300

    created transaction repository

---
+++ src/transactions/repositories/transactionRepository.ts
@@ -0,0 +1,9 @@
+import { Transaction } from "../entities/transaction";
+
+export interface ITransactionRepository {
+  fetchTransactions(): Promise<TransactionFetchResult[]>;
+}
+export type TransactionFetchResult = {
+  data: Transaction[];
+  error?: string;
+};
